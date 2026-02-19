90ce73f5b73d25532c872d7e401a6b5a6bc923d3
elmandadashov-arch <elmandadashov@gmail.com>
2026-02-20 02:05:59 +0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,13 +1,13 @@
-self.addEventListener("install", e => {
+self.addEventListener("install", (e) => {
   e.waitUntil(
     caches.open("v1").then(cache =>
-      cache.addAll(["./","./index.html","./manifest.json"])
+      cache.addAll(["/", "/index.html", "/manifest.json", "/sw.js"])
     )
   );
 });
 
-self.addEventListener("fetch", e => {
+self.addEventListener("fetch", (e) => {
   e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
+    caches.match(e.request).then(resp => resp || fetch(e.request))
   );
 });
